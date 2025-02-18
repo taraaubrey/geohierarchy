9df6a50fc582acbe3184ebf99d5edc29de201650
taraaubrey <tara.a.forstner@gmail.com>
2025-02-18 17:59:42 +1300

    major refactoring

--- examples/parsing_root.py
+++ examples/parsing_root.py
@@ -1,22 +1,10 @@
 from geoconfig.main_config.UserConfig import UserConfig
-from geoconfig.main_config.SchemaConfig import SchemaConfig
-
-from geoconfig.user_input.user_input_factory import UserInputFactory
-from geoconfig.user_input.input_types import (
-    ValueInput,
-    FilepathInput,
-    CachedInput,
-    PythonModuleInput,
-    MultiInput,
-    MathInput,
-)
 
 
 user_input_filepath = r'examples\example_input.yaml'
 
-# internal schemas
-schema_filepaths = [r'src\geoconfig\settings\input_schema.yaml']
+model_input = UserConfig.from_filepath(filepath=user_input_filepath)
 
-main_spec = UserConfig.from_filepath(filepath=user_input_filepath)
+
 
 print('here')
--- src/geoconfig/settings/config.py
+++ src/geoconfig/settings/config.py
@@ -1,14 +1,27 @@
 
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
 model_type_key = ['model_config', 'model_type']
 upstream_model_keys = ['input_hierarchy', 'models']
 
-@dataclass
-class ConfigSettings:
-    model_type_key: list[str]
-    upstream_model_keys: list[str]
+# cache seetings
+input_sources_keys = ['input_sources']
+input_cache_keys = ['input_cache']
 
-    def __post_init__(self):
-        self.model_type_key = model_type_key
-        self.upstream_model_keys = upstream_model_keys
+# model parameters
+
+
+# schema filepaths
+schema_filepaths = [r'src\geoconfig\settings\input_schema.yaml']
+
+
+# @dataclass
+# class ConfigSettings:
+#     model_type_key: list[str]
+#     upstream_model_keys: list[str]
+#     schemas: list[str]
+
+#     def __post_init__(self):
+#         self.model_type_key = model_type_key
+#         self.upstream_model_keys = upstream_model_keys
+#         self.schemas = schema_filepaths
